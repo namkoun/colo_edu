aeddb09ae32ec02d8d55e983e03ddb6213a2f105
Kim Juho <sv05159@naver.com>
2021-08-03 11:33:22 +0900

    update content message

--- src/main/java/kr/cfms/dashboard/controller/ajax/DashboardAjaxController.java
+++ src/main/java/kr/cfms/dashboard/controller/ajax/DashboardAjaxController.java
@@ -30,10 +30,9 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("ajax/dashboard")
 @RequiredArgsConstructor
 public class DashboardAjaxController {
-	
+
 	private final NoticeService noticeService;
 	private final DashboardService dashboardService;
-	
 
 	@GetMapping("get/list")
 	public ResponseEntity<List<NoticeDTO>> getNoticeList(@ModelAttribute NoticeSearchDTO noticeSearchDTO) {
@@ -45,7 +44,7 @@ public class DashboardAjaxController {
 	@GetMapping("get/searchDashboardOut")
 	public ResponseEntity<List<DashboardResultDTO>> searchDashboardOut(HttpSession session,
 															@ModelAttribute DashboardSearchDTO dashboardSearchDTO){
-		
+
 		UserInfo userInfo = (UserInfo) session.getAttribute("userInfo");
 		dashboardSearchDTO.setMid(userInfo.getMid());
 		List<DashboardResultDTO> dashboardOutList;
@@ -53,105 +52,109 @@ public class DashboardAjaxController {
 		return ResponseEntity.ok(dashboardOutList);
 	}
 
-	//대쉬보드 입고현황 호출 
+	//대쉬보드 입고현황 호출
 	@GetMapping("get/searchDashboardIn")
 	public ResponseEntity<List<DashboardResultDTO>> searchDashboardIn(HttpSession session,
 															@ModelAttribute DashboardSearchDTO dashboardSearchDTO){
-		
+
 		UserInfo userInfo = (UserInfo) session.getAttribute("userInfo");
 		dashboardSearchDTO.setMid(userInfo.getMid());
 		List<DashboardResultDTO> dashboardInList;
 		dashboardInList = dashboardService.selectDashboardIn(dashboardSearchDTO);
 		return ResponseEntity.ok(dashboardInList);
 	}
-	
+
 	//대쉬보드 보관량현황 호출
 	@GetMapping("get/searchDashboardStoreWeight")
 	public ResponseEntity<List<DashboardResultDTO>> searchDashboardStoreWeight(HttpSession session,
 															@ModelAttribute DashboardSearchDTO dashboardSearchDTO){
-		
+
 		UserInfo userInfo = (UserInfo) session.getAttribute("userInfo");
 		dashboardSearchDTO.setMid(userInfo.getMid());
-		
+
 		List<DashboardResultDTO> dashboardStoreWeightList;
 		dashboardStoreWeightList = dashboardService.selectDashboardStoreWeight(dashboardSearchDTO);
 		return ResponseEntity.ok(dashboardStoreWeightList);
 	}
-	
-	//대쉬보드 출고순위 현황 호출 
+
+	//대쉬보드 출고순위 현황 호출
 	@GetMapping("get/searchDashboardGoodsRank")
 	public ResponseEntity<List<DashboardResultDTO>> searchDashboardGoodsRank(HttpSession session,
 															@ModelAttribute DashboardSearchDTO dashboardSearchDTO){
-		
+
 		UserInfo userInfo = (UserInfo) session.getAttribute("userInfo");
 		dashboardSearchDTO.setMid(userInfo.getMid());
 		List<DashboardResultDTO> dashboardGoodsRankList;
 		dashboardGoodsRankList = dashboardService.selectDashboardGoodsRank(dashboardSearchDTO);
 		return ResponseEntity.ok(dashboardGoodsRankList);
 	}
-	
-	//대쉬보드 출고순위 상품별 차트 데이터 호출 
+
+	//대쉬보드 출고순위 상품별 차트 데이터 호출
 	@GetMapping("get/searchDashboardGoodsRankRange")
 	public ResponseEntity<List<DashboardResultDTO>> searchDashboardGoodsRankRange(@ModelAttribute DashboardSearchDTO dashboardSearchDTO){
-		
+
 		List<DashboardResultDTO> dashboardGoodsRankRangeList;
 		dashboardGoodsRankRangeList = dashboardService.selectDashboardGoodsRankRange(dashboardSearchDTO);
 		return ResponseEntity.ok(dashboardGoodsRankRangeList);
 	}
-	
-	//대쉬보드 재고부족현황 호출 
+
+	//대쉬보드 재고부족현황 호출
 	@GetMapping("get/searchDashboardStockLack")
 	public ResponseEntity<List<DashboardResultDTO>> searchDashboardStockLack(HttpSession session,
 															@ModelAttribute DashboardSearchDTO dashboardSearchDTO){
-		
+
 		UserInfo userInfo = (UserInfo) session.getAttribute("userInfo");
 		dashboardSearchDTO.setMid(userInfo.getMid());
-		
+
 		List<DashboardResultDTO> dashboardStockLackList;
 		dashboardStockLackList = dashboardService.selectDashboardStockLack(dashboardSearchDTO);
 		return ResponseEntity.ok(dashboardStockLackList);
 	}
-	
+
+
+	/**
+	 * 공지사항
+	 */
 	@GetMapping("get/preNextDoc")
 	public List<NoticeDTO> preNextDoc(@RequestParam("id") long id) {
 		List<NoticeDTO> preNextDocResult;
 		preNextDocResult = noticeService.preNextDoc(id);
 		return preNextDocResult;
 	}
-	
+
 	//공지사항 등록
 	@PostMapping("add/insertNotice")
 	public ResponseEntity<MessageVo> insertNotice(HttpSession session, @RequestBody NoticeVO noticeVO) {
-		
+
 		UserInfo userInfo = (UserInfo) session.getAttribute("userInfo");
 		noticeVO.setRegMid(userInfo.getMid());
-		
+
 		noticeService.insertNotice(noticeVO);
-				
-		return ResponseEntity.ok(new MessageVo("공지사항 등록 완료"));	 
+
+		return ResponseEntity.ok(new MessageVo("공지사항 등록 완료"));
 	}
-	
+
 	//공지사항 수정
 	@PostMapping("add/updateNotice")
 	public ResponseEntity<MessageVo> updateNotice(HttpSession session, @RequestBody NoticeVO noticeVO) {
-		
+
 		UserInfo userInfo = (UserInfo) session.getAttribute("userInfo");
 		noticeVO.setRegMid(userInfo.getMid());
-		
+
 		noticeService.updateNotice(noticeVO);
-				
-		return ResponseEntity.ok(new MessageVo("공지사항 수정 완료"));	 
+
+		return ResponseEntity.ok(new MessageVo("공지사항 수정 완료"));
 	}
-	
+
 	//공지사항 삭제
 	@PostMapping("add/deleteNotice")
 	public ResponseEntity<MessageVo> deleteNotice(HttpSession session, @RequestBody NoticeVO noticeVO) {
-		
+
 		UserInfo userInfo = (UserInfo) session.getAttribute("userInfo");
 		noticeVO.setRegMid(userInfo.getMid());
-		
+
 		noticeService.deleteNotice(noticeVO);
-				
-		return ResponseEntity.ok(new MessageVo("공지사항 삭제 완료"));	 
+
+		return ResponseEntity.ok(new MessageVo("공지사항 삭제 완료"));
 	}
 }
